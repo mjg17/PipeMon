e83fa59d6c299d54ed4fd93e0cb72fbf03d89011
Michael Gray <mg13@sanger.ac.uk>
2011-03-29 14:32:23 +0100

    First cut.
    Not very happy with it - need to make Model::Otter::MFetcher
    a per-context model too.

--- lib/PipeMon/Model/PipeForSpecies.pm
+++ lib/PipeMon/Model/PipeForSpecies.pm
@@ -2,7 +2,41 @@ package PipeMon::Model::PipeForSpecies;
 use Moose;
 use namespace::autoclean;
 
-extends 'Catalyst::Model';
+use Bio::Otter::MFetcher;
+use Data::Dumper;
+
+extends 'Catalyst::Model::DBIC::Schema';
+with 'Catalyst::Component::InstancePerContext';
+
+__PACKAGE__->config(
+    schema_class => 'PipeMon::Schema::Pipeline',
+    connect_info => 'dbi:mysql:pipe_not_set', # to stop complaints at startup
+    );
+
+sub build_per_context_instance {
+    my ($self, $c) = @_;
+
+    my $new = bless({ %$self }, ref($self));
+
+    my $mfetcher = Bio::Otter::MFetcher->new();
+    $mfetcher->species_dat_filename('/nfs/WWWdev/SANGER_docs/data/otter/54/species.dat');
+    $mfetcher->dataset_name($c->stash->{species});
+
+    my $opt_str = $mfetcher->satellite_dba_options('pipeline_db_head');
+
+    # Duplication with Bio::Otter::MFetcher
+    my %options;
+    {
+        %options = eval $opt_str;
+    }
+    $c->log->debug( %options ? join(',', %options) : "NO PIPELINE" );
+
+    my $dsn = sprintf("dbi:mysql:host=%s;port=%s;database=%s",
+                      $options{-host}, $options{-port}, $options{-dbname});
+
+    $new->schema($self->schema->connect( $dsn, $options{-user} ));
+    return $new;
+} 
 
 =head1 NAME
 
