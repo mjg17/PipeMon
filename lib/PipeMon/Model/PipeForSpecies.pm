9a93f003347bc8eb93dbc64fcbf8a623b3d3a867
Michael Gray <mg13@sanger.ac.uk>
2011-03-29 09:49:03 +0100

    Ran: script/pipemon_create.pl Model PipeForSpecies

---
+++ lib/PipeMon/Model/PipeForSpecies.pm
@@ -0,0 +1,28 @@
+package PipeMon::Model::PipeForSpecies;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::Model';
+
+=head1 NAME
+
+PipeMon::Model::PipeForSpecies - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+Michael Gray
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
