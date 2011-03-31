f4d3383114b5f101441affd5afc9ac6c89d3388a
Michael Gray <mg13@sanger.ac.uk>
2011-03-31 11:43:28 +0100

    Detect analysis_id and logic_name.

--- lib/PipeMon/Controller/Analysis.pm
+++ lib/PipeMon/Controller/Analysis.pm
@@ -17,16 +17,53 @@ Catalyst Controller.
 =cut
 
 
+=head2 base
+
+Just gets us chained to the right place with the right pathpart initially
+
+=cut
+
+sub base :Chained('/species/base') :PathPart('analysis') :CaptureArgs(0) {
+    my ( $self, $c ) = @_;
+}
+
 =head2 index
 
 =cut
 
-sub index :Path :Args(0) {
+sub index :Chained('base') :PathPart('') :Args(0) {
     my ( $self, $c ) = @_;
 
     $c->response->body('Matched PipeMon::Controller::Analysis in Analysis.');
 }
 
+=head2 analysis
+
+=cut
+
+sub analysis :Chained('base') :PathPart('') :Args(1) {
+    my ( $self, $c, $key ) = @_;
+
+    my $resultset = $c->model('PipeForSpecies::Analysis');
+
+    my $analysis;
+    if ($key =~ /^\d+$/) {
+        $analysis = $resultset->find($key);
+    } elsif ($key =~ /^[[:alpha:]]\w*$/) {
+        ($analysis) = $resultset->search({logic_name => $key});
+    } else {
+        # Bad format
+    }
+    my $analysis_id = "<NOT FOUND>";
+    my $logic_name  = "<NOT FOUND>";
+
+    if ($analysis) {
+        $analysis_id = $analysis->analysis_id;
+        $logic_name  = $analysis->logic_name;
+    }
+
+    $c->response->body("Got key: $key, analysis_id: $analysis_id, logic_name: $logic_name");
+}
 
 =head1 AUTHOR
 
