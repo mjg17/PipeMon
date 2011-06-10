89768479fb81718cad78bb02a9dfb8686bb666df
Michael Gray <mg13@sanger.ac.uk>
2011-06-10 15:48:18 +0100

    Get all chromosomes

--- lib/PipeMon/Controller/SeqRegion.pm
+++ lib/PipeMon/Controller/SeqRegion.pm
@@ -36,7 +36,15 @@ sub seq_sets :Chained('base') :PathPart('seq_sets') :Args(0) {
     my ( $self, $c ) = @_;
 
     my $resultset = $c->stash->{seq_region_rs};
-    my $seq_sets  = $resultset->search( undef, { rows => 20 } );
+    my $seq_sets  = $resultset->search(
+        {
+            'coord_system.name' => 'chromosome',
+        },
+        {
+            join => ['coord_system' ],
+            order_by => 'me.name',
+        } 
+        );
 
     $c->stash( seq_sets => $seq_sets,
                template => 'seq_region/seq_sets.tt2',
