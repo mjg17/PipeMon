aa7c49a08127268164d726bff0c82610fd63434a
Michael Gray <mg13@sanger.ac.uk>
2011-06-10 15:06:14 +0100

    Flesh out seq_sets

--- lib/PipeMon/Controller/SeqRegion.pm
+++ lib/PipeMon/Controller/SeqRegion.pm
@@ -16,17 +16,39 @@ Catalyst Controller.
 
 =cut
 
+=head2 base
 
-=head2 index
+Just gets us chained to the right place with the right pathpart initially
 
 =cut
 
-sub index :Path :Args(0) {
+sub base :Chained('/loutreorpipe/base') :PathPart('') :CaptureArgs(0) {
     my ( $self, $c ) = @_;
-
-    $c->response->body('Matched PipeMon::Controller::SeqRegion in SeqRegion.');
+    my $model = $c->stash->{db_model};
+    $c->stash( seq_region_rs => $model->resultset('SeqRegion') );
 }
 
+=head2 seq_sets
+
+=cut
+
+sub seq_sets :Chained('base') :PathPart('seq_sets') :Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $resultset = $c->stash->{seq_region_rs};
+    my $seq_sets  = $resultset->search( undef, { rows => 20 } );
+
+    $c->stash( seq_sets => $seq_sets,
+               template => 'seq_region/seq_sets.tt2',
+        );
+}
+
+=head2 seq_region 
+
+=cut
+
+sub seq_region :Chained('base') :PathPart('seq_region') :Args(1) {
+}
 
 =head1 AUTHOR
 
