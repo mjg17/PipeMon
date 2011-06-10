e272496f09da835e8891105614b06e38d6a4d218
Michael Gray <mg13@sanger.ac.uk>
2011-06-10 13:56:46 +0100

    Ran script/pipemon_create.pl Controller SeqRegion

---
+++ lib/PipeMon/Controller/SeqRegion.pm
@@ -0,0 +1,44 @@
+package PipeMon::Controller::SeqRegion;
+use Moose;
+use namespace::autoclean;
+
+BEGIN {extends 'Catalyst::Controller'; }
+
+=head1 NAME
+
+PipeMon::Controller::SeqRegion - Catalyst Controller
+
+=head1 DESCRIPTION
+
+Catalyst Controller.
+
+=head1 METHODS
+
+=cut
+
+
+=head2 index
+
+=cut
+
+sub index :Path :Args(0) {
+    my ( $self, $c ) = @_;
+
+    $c->response->body('Matched PipeMon::Controller::SeqRegion in SeqRegion.');
+}
+
+
+=head1 AUTHOR
+
+Michael Gray
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
