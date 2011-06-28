ac3484dfe20c7e3ac850255163cbdb2133916dc0
Michael Gray <mg13@sanger.ac.uk>
2011-06-28 16:54:10 +0100

    Get input_id view and basic links working.

--- lib/PipeMon/Controller/InputId.pm
+++ lib/PipeMon/Controller/InputId.pm
@@ -17,16 +17,59 @@ Catalyst Controller.
 =cut
 
 
-=head2 index
+=head2 base
+
+Just gets us chained to the right place with the right pathpart initially
 
 =cut
 
-sub index :Path :Args(0) {
+sub base :Chained('/species/base') :PathPart('') :CaptureArgs(0) {
     my ( $self, $c ) = @_;
-
-    $c->response->body('Matched PipeMon::Controller::InputId in InputId.');
+    $c->stash( input_id_analysis_rs => $c->model('PipeForSpecies::InputIdAnalysis') );
 }
 
+=head2 input_id
+
+=cut
+
+sub input_id :Chained('base') :PathPart('input_id') :Args(1) {
+    my ( $self, $c, $key ) = @_;
+
+    my $resultset = $c->stash->{input_id_analysis_rs};
+
+    my $input_id;
+    if ($key =~ /^[[:alnum:]:.]+$/) {
+        $input_id = $resultset->search( 
+            {
+                'me.input_id'         => $key,
+                'analysis.logic_name' => { like => 'Submit%' },
+            },
+            {
+                join => 'analysis'
+            }
+            )->single;
+    } else {
+        # Bad format
+        $c->response->status(400);
+        $c->response->body("'$key' doesn't look like an input_id");
+        $c->detach;
+    }
+
+    unless ($input_id) {
+        $c->response->status(404);
+        $c->response->body("No such input_id '$key'");
+        $c->detach;
+    }
+
+    my $jobs     = $input_id->jobs_by_input_id;
+    my $analyses = $input_id->analyses_by_input_id;
+
+    $c->stash( input_id   => $input_id,
+               jobs       => $jobs,
+               analyses   => $analyses,
+               template   => 'input_id/input_id.tt2',
+        );
+}
 
 =head1 AUTHOR
 
