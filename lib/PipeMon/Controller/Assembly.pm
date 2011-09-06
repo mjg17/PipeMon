191e662fb58c235d326a7068dea2496f3fad1442
Michael Gray <mg13@sanger.ac.uk>
2011-09-06 17:19:16 +0100

    Provide assembly components.

--- lib/PipeMon/Controller/Assembly.pm
+++ lib/PipeMon/Controller/Assembly.pm
@@ -17,14 +17,46 @@ Catalyst Controller.
 =cut
 
 
-=head2 index
+=head2 base
+
+Just gets us chained to the right place with the right pathpart initially
 
 =cut
 
-sub index :Path :Args(0) {
+sub base :Chained('/loutreorpipe/base') :PathPart('') :CaptureArgs(0) {
     my ( $self, $c ) = @_;
+    my $model = $c->stash->{db_model};
+    $c->stash( assembly_rs => $model->resultset('Assembly') );
+}
 
-    $c->response->body('Matched PipeMon::Controller::Assembly in Assembly.');
+=head2 components
+
+=cut
+
+sub components :Chained('base') :PathPart('components') :Args(1) {
+    my ( $self, $c, $assembly_sr_id ) = @_;
+
+    unless ($assembly_sr_id =~ /^\d+$/) {
+        # Bad format
+        $c->response->status(400);
+        $c->response->body("'$assembly_sr_id' doesn't look like a seq_region_id");
+        $c->detach;
+    }
+
+    my $cmp_rs = $c->stash->{assembly_rs}->search(
+        { asm_seq_region_id => $assembly_sr_id },
+        {
+            prefetch => [ qw(assembly component) ],
+            order_by => 'asm_start',
+        },
+        );
+
+    my $first = $cmp_rs->first;
+    $c->stash(
+        cmp_rs   => $cmp_rs,
+        template => 'assembly/components.tt2',
+        assembly => $first ? $first->assembly : { name => "{${assembly_sr_id}}" },
+        );
 }
 
 
