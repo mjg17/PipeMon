c1fac601f34209f9581a15396dbb333fae2bfff3
Michael Gray <mg13@sanger.ac.uk>
2011-10-12 15:42:43 +0100

    First cut at CoordSystem controller and template.

--- lib/PipeMon/Controller/CoordSystem.pm
+++ lib/PipeMon/Controller/CoordSystem.pm
@@ -4,6 +4,22 @@ use namespace::autoclean;
 
 BEGIN {extends 'Catalyst::Controller'; }
 
+has 'coord_system_keys' => (
+    is  => 'rw',
+    isa => 'ArrayRef[Str]',
+);
+
+__PACKAGE__->config(
+    coord_system_keys => [ qw(
+        coord_system_id
+        species_id
+        name
+        version
+        rank
+        attrib
+    )],
+    );
+
 =head1 NAME
 
 PipeMon::Controller::CoordSystem - Catalyst Controller
@@ -17,14 +33,36 @@ Catalyst Controller.
 =cut
 
 
-=head2 index
+=head2 base
+
+Just gets us chained to the right place with the right pathpart initially
 
 =cut
 
-sub index :Path :Args(0) {
+sub base :Chained('/loutreorpipe/base') :PathPart('') :CaptureArgs(0) {
     my ( $self, $c ) = @_;
+    my $model = $c->stash->{db_model};
+    $c->stash( coord_system_rs => $model->resultset('CoordSystem') );
+}
 
-    $c->response->body('Matched PipeMon::Controller::CoordSystem in CoordSystem.');
+=head2 coord_system
+
+=cut
+
+sub coord_system :Chained('base') :PathPart('coord_system/id') :Args(1) {
+    my ( $self, $c, $id ) = @_;
+
+    my $coord_system = $c->stash->{coord_system_rs}->find($id);
+    unless ($coord_system) {
+        $c->response->status(404);
+        $c->response->body("No such coord_system_id '$id'");
+        $c->detach;
+    }
+
+    $c->stash( coord_system => $coord_system,
+               keys         => $self->coord_system_keys,
+               template     => 'coord_system/coord_system.tt2',
+        );
 }
 
 
