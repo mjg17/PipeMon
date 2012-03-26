b70bd1a41cae9f2658f4e412ec57407a04d498dc
Michael Gray <mg13@sanger.ac.uk>
2012-03-26 16:13:02 +0100

    List meta table entries.

--- lib/PipeMon/Controller/Meta.pm
+++ lib/PipeMon/Controller/Meta.pm
@@ -17,16 +17,35 @@ Catalyst Controller.
 =cut
 
 
-=head2 index
+=head2 base
+
+Just gets us chained to the right place with the right pathpart initially
 
 =cut
 
-sub index :Path :Args(0) {
+sub base :Chained('/loutreorpipe/base') :PathPart('') :CaptureArgs(0) {
     my ( $self, $c ) = @_;
-
-    $c->response->body('Matched PipeMon::Controller::Meta in Meta.');
+    my $model = $c->stash->{db_model};
+    $c->stash( meta_rs => $model->resultset('Meta') );
 }
 
+=head2 all
+
+=cut
+
+sub all :Chained('base') :PathPart('all') :Args(0) {
+    my ( $self, $c ) = @_;
+
+    my $resultset = $c->stash->{meta_rs};
+    my $sorted = $resultset->search(
+        undef,
+        { order_by => [ qw(species_id meta_key meta_value) ] },
+        );
+
+    $c->stash( meta_t   => [$sorted->all],
+               template => 'meta/all.tt2',
+        );
+}
 
 =head1 AUTHOR
 
