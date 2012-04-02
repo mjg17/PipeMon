aff93e5e2f66881a88679cbe1b4b374412767533
Michael Gray <mg13@sanger.ac.uk>
2012-04-02 11:13:22 +0100

    Extra PERL5LIB entries for my Mac environment.

---
+++ etc/MacOS.rc.sh
@@ -0,0 +1,8 @@
+export PERL5LIB=$PERL5LIB\
+:~/Work/git/ensembl-otter/modules\
+:~/Work/cvs/anacode/PerlModules\
+:~/Work/cvs/ensembl/ensembl/modules\
+:~/Dist/otterlace.app/Contents/Resources/lib/perl5/site_perl/5.12.3
+
+# EOF
+
